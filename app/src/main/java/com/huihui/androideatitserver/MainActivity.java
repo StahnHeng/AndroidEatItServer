116392a14fead8978529b609fb1dee8ac8df6734
Stahnheng <stahncad@gmail.com>
2017-10-10 07:10:46 +0800

    part8-server1

--- app/src/main/java/com/huihui/androideatitserver/MainActivity.java
+++ app/src/main/java/com/huihui/androideatitserver/MainActivity.java
@@ -1,13 +1,27 @@
 package com.huihui.androideatitserver;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.ttf");
+        txtSlogan.setTypeface(face);
+
+        
+
     }
 }
